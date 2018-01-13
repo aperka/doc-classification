b1dfc335d295ed4f2043bb13203b4a30c9e6cf93
Arkadiusz Perka <arkadiusz@MacBook-Pro-Arkadiusz.local>
2018-01-14 00:07:57 +0100

    ADD: missing files

---
+++ project_utils.py
@@ -0,0 +1,16 @@
+from nltk.corpus import stopwords
+from nltk import word_tokenize
+from nltk.stem.porter import PorterStemmer
+import re
+
+cachedStopWords = stopwords.words("english")
+
+
+def tokenize(text):
+    min_length = 3
+    words = map(lambda word: word.lower(), word_tokenize(text))
+    words = [word for word in words if word not in cachedStopWords]
+    tokens = (list(map(lambda token: PorterStemmer().stem(token), words)))
+    p = re.compile('[a-zA-Z]+')
+    filtered_tokens = list(filter(lambda token: p.match(token) and len(token) >= min_length, tokens))
+    return filtered_tokens
