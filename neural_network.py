5b3d4f6610fad3e7d792eebf4795fc11a29ad13f
Aleksandra <earrendil@gmail.com>
2018-01-15 19:09:46 +0100

    add parameters to change

--- neural_network.py
+++ neural_network.py
@@ -10,6 +10,6 @@ def nn_scale_data(train_data, test_data):
     test_scaled = scaler.transform(test_data)
     return train_scaled, test_scaled
 
-def nn_create(hidden_layer_sizes=(30,), alpha=1e-05, epsilon=1e-08):
-    mlp = MLPClassifier(hidden_layer_sizes=hidden_layer_sizes, alpha=alpha, epsilon=epsilon )
+def nn_create(hidden_layer_sizes=(5000,), alpha=1e-01, epsilon=1e-08, solver='adam', max_iter=200):
+    mlp = MLPClassifier(hidden_layer_sizes=hidden_layer_sizes, alpha=alpha, epsilon=epsilon, solver=solver, max_iter=max_iter )
     return mlp
